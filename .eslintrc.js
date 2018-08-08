abfb1a86030a306995d07f3663ececf50a0707ac
Billy Vong <billy@sentry.io>
2018-08-08 15:33:06 -0700

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: "eslint:recommended",
+  parser: "babel-eslint",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2018
+  },
+  rules: {
+    "no-console": ["off"]
+  }
+};
